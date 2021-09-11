80c0e18107ccd848bb0abc76c1e526ee822b47f9
wangmengnan <acbool@lonlife.cn>
2021-09-11 11:14:55 +0800

    init project

---
+++ FakeWeChat/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FakeWeChat
+//
+//  Created by lonlife on 2021/9/11.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
